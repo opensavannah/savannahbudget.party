6a05038a4738dabcb8ed1fb1f326b9ebce0cbb16
Carl V. Lewis <carl@carlvlewis.net>
2018-09-10 15:39:39 -0400

    align departments and services

--- app/src/data/departments.js
+++ app/src/data/departments.js
@@ -6,9 +6,10 @@ const departments = [
       'The mission of the Municipal Courts is to provide fair, efficient, and accountable service to the public by impartially administering justice so that the quality of life is enhanced.\n\n  Municipal court and Downtown Austin Community Court make up the judicial branch of the City of Austin local government. The court oversees cases of traffic, city ordinance, state code, and parking violations.',
     amount: null,
     explainYourSpending: null,
-    lastYearAmount: 22514014,
-    amount2016: 22514014,
+    lastYearAmount: 27576596, // City Manager Proposed Budget
+    amount2018: 27576596,
     amount2017: 22514014,
+    amount2016: 22514014,
     percentChange: 0,
     deptId: 1,
     url: 'https://data.austintexas.gov/stories/s/Municipal-Court/xm6x-8vij',
@@ -29,9 +30,10 @@ const departments = [
       'The mission of the Police Department is to keep you, your family and our community safe.\n\n Officers do everything from enforcing the law, to preventing crime, investigating crime, reaching out to the community and writing reports',
     amount: null,
     explainYourSpending: null,
-    lastYearAmount: 386573423,
-    amount2016: 373570249,
+    lastYearAmount: 415749070,
+    amount2018: 415749070,
     amount2017: 386573423,
+    amount2016: 373570249,
     percentChange: 0,
     deptId: 2,
     url: 'https://data.austintexas.gov/stories/s/Police/v4d4-74jw',
@@ -48,7 +50,8 @@ const departments = [
       'The Austin Fire Department is committed to creating safer communities through prevention, preparedness and effective emergency response.\n\n ',
     amount: null,
     explainYourSpending: null,
-    lastYearAmount: 189013831,
+    lastYearAmount: 198478600,
+    amount2018: 198478600,
     amount2016: 175266499,
     amount2017: 189013831,
     percentChange: 0,
@@ -71,9 +74,10 @@ const departments = [
       'The mission of Austin-Travis County EMS is to positively impact and change the lives of the people we serve through the personal commitment of each of our members to provide great patient and customer care, pursue excellence in everything we do, and show compassion to others every day.\n\n EMS provides emergency communication services, ground and air emergency ambulance response, standby support, and special rescue services to the community.',
     amount: null,
     explainYourSpending: null,
-    lastYearAmount: 83786011,
-    amount2016: 78255956,
+    lastYearAmount: 88336542,
+    amount2018: 88336542,
     amount2017: 83786011,
+    amount2016: 78255956,
     percentChange: 0,
     deptId: 4,
     tradeoff: [
@@ -84,8 +88,7 @@ const departments = [
     metrics: [
       'Percent of potentially life-threatening calls responded to within 9 minutes and 59 seconds',
     ],
-    url:
-      'https://data.austintexas.gov/stories/s/Emergency-Medical-Services/nq44-vyqb',
+    url: 'https://data.austintexas.gov/stories/s/Emergency-Medical-Services/nq44-vyqb',
   },
   {
     name: 'Planning & Zoning',
@@ -95,35 +98,12 @@ const departments = [
     url: 'https://data.austintexas.gov/stories/s/Planning-and-Zoning/se7h-anvc',
     amount: null,
     explainYourSpending: null,
-    lastYearAmount: 7642545,
-    amount2016: 7505329,
+    lastYearAmount: 9043645,
+    amount2018: 9043645,
     amount2017: 7642545,
+    amount2016: 7505329,
     percentChange: 0,
     deptId: 5,
-  },
-  {
-    name: 'Development Services',
-    description:
-      'Consolidates the process of land development permitting and assistance',
-    learnMore:
-      'Development Services provides a mix of services to foster economic vibrancy, ensure the quality of new development and preserve our natural resources.\n\n The mission of the Department is to provide comprehensive development review and inspection services to build a better Austin.\n\n  Building Plan Review is responsible for reviewing all construction plans for compliance with the Land Development Code before building permits can be issued. The Building Inspection Division performs construction related inspections for all buildings within the City limits, and electrical and plumbing services in the extra-territorial jurisdiction.',
-    amount: null,
-    explainYourSpending: null,
-    lastYearAmount: 37523998,
-    amount2016: 32902297,
-    amount2017: 37523998,
-    percentChange: 0,
-    deptId: 6,
-    tradeoff: [
-      {
-        68764: '1 FTE for land-use permit review',
-      },
-    ],
-    metrics: [
-      'Percent of building inspections performed within 24 hours of request',
-    ],
-    url:
-      'https://data.austintexas.gov/stories/s/Development-Services/qwps-5848',
   },
   {
     name: 'Animal Services',
@@ -132,11 +112,12 @@ const departments = [
       'The mission of the Austin Animal Services Department is to prevent animal homelessness and promote humane, compassionate treatment of animals by enforcing regulations, providing a safety net for lost or homeless animals, and achieving live outcomes for at least 90% of sheltered pets.\n\n  The departments provides an array of animal control, health, and pet-owner services to the public. They provide food, water, shelter and veterinary care for lost or homeless animals. They enforce animal regulations including impoundment, quarantine and other rabies control services.',
     amount: null,
     explainYourSpending: null,
-    lastYearAmount: 12914285,
-    amount2016: 11546362,
+    lastYearAmount: 13911617,
+    amount2018: 13911617,
     amount2017: 12914285,
+    amount2016: 11546362,
     percentChange: 0,
-    deptId: 7,
+    deptId: 6,
     tradeoff: [
       {
         900: '10 microchipped animals',
@@ -149,19 +130,18 @@ const departments = [
   },
   {
     name: 'Parks & Recreation',
-    description:
-      'Providing, protecting, and preserving the city’s park system.',
+    description: 'Providing, protecting, and preserving the city’s park system.',
     learnMore:
       'The mission of the Parks and Recreation Department is to provide, protect, and preserve a premier park system that promotes quality recreational, cultural, and outdoor experiences for Austins citizens and visitors.\n\n  Parks and Recreation department is tasked with providing, protecting, and preserving the city’s park system',
     amount: null,
     explainYourSpending: null,
-    lastYearAmount: 80504938,
-    amount2016: 75432093,
+    lastYearAmount: 91862883,
+    amount2018: 91862883,
     amount2017: 80504938,
+    amount2016: 75432093,
     percentChange: 0,
-    deptId: 8,
-    url:
-      'https://data.austintexas.gov/stories/s/Parks-and-Recreation/wgi2-kbuk',
+    deptId: 7,
+    url: 'https://data.austintexas.gov/stories/s/Parks-and-Recreation/wgi2-kbuk',
     tradeoff: [
       {
         75000: '1 FTE for maintaining safe trails and playgrounds',
@@ -179,29 +159,29 @@ const departments = [
       },
     ],
     metrics: ['Library Program attendance per capita'],
-    url:
-      'https://data.austintexas.gov/stories/s/Austin-Public-Library/patt-h8xx',
+    url: 'https://data.austintexas.gov/stories/s/Austin-Public-Library/patt-h8xx',
     amount: null,
     explainYourSpending: null,
-    lastYearAmount: 47167036,
-    amount2016: 41755192,
+    lastYearAmount: 52168472,
+    amount2018: 52168472,
     amount2017: 47167036,
+    amount2016: 41755192,
     percentChange: 0,
-    deptId: 9,
+    deptId: 8,
   },
   {
     name: 'Public Health',
-    description:
-      'Disease prevention, promoting and protecting community health',
+    description: 'Disease prevention, promoting and protecting community health',
     learnMore:
       'Mission is to prevent disease, and promote and protect the health and well-being of the community by providing services such as immunizations, food, shelter, and job assistance, health screenings, and overseeing the inspection and permitting of restaurants',
     amount: null,
     explainYourSpending: null,
-    lastYearAmount: 70260911,
-    amount2016: 61282968,
+    lastYearAmount: 43631494,
+    amount2018: 43631494,
     amount2017: 70260911,
+    amount2016: 61282968,
     percentChange: 0,
-    deptId: 10,
+    deptId: 9,
     tradeoff: [
       {
         4600: 'Shot for tots for 10 individuals',
@@ -214,17 +194,17 @@ const departments = [
   },
   {
     name: 'Neighborhood Housing & Community Development',
-    description:
-      'Housing, small business assistance, renter assistance, and more',
+    description: 'Housing, small business assistance, renter assistance, and more',
     learnMore:
       'The purpose of the Neighborhood Housing and Community Development Department is to provide housing, community and small business development services to benefit eligible residents so they can have access to livable neighborhoods and can increase their opportunities for self-sufficiency.\n\n The Neighborhood Housing & Community Development programs  include housing, small business assistance, renter assistance, and neighborhood and community revitalization.',
     amount: null,
     explainYourSpending: null,
-    lastYearAmount: 5798181,
-    amount2016: 4818944,
+    lastYearAmount: 7748100,
+    amount2018: 7748100,
     amount2017: 5798181,
+    amount2016: 4818944,
     percentChange: 0,
-    deptId: 11,
+    deptId: 10,
     tradeoff: [
       {
         3400: '1 child receives youth services',
@@ -235,6 +215,68 @@ const departments = [
     ],
     url:
       'https://data.austintexas.gov/stories/s/Neighborhood-Housing-and-Community-Development/wywe-6mv3',
+  },
+  {
+    name: 'Social Service Contracts',
+    description:
+      'Programs with partner agencies and non-profits addressing children & youth, homelessness, behavioral health, workforce development, community planning, and HIV prevention.',
+    learnMore:
+      'Social Service contracts represent agreements for program funding for partner agencies and non-profits addressing children & youth, homelessness, behavioral health, workforce development, community planning, and HIV prevention. ',
+    learnMoreUrl:
+      'http://budget.austintexas.gov/#!/year/2017/operating/0/fund_nm/General+Fund/0/dept_nm/Austin+Public+Health/0/div_nm/Social+Services+Contracts/0/gp_nm/Basic+Needs/0/unit_nm/Basic+Needs+Contracts/0/obj_cat?vis=barChart',
+    amount: null,
+    explainYourSpending: null,
+    lastYearAmount: 35694422,
+    amount2018: 35694422,
+    percentChange: 0,
+    deptId: 11,
+  },
+  {
+    name: 'Human Resources',
+    description: 'Trainings and Tuition Reimbursements for City staff professional development',
+    learnMore: 'Learn more about Human Resources training: ',
+    learnMoreUrl:
+      'http://budget.austintexas.gov/#!/year/2017/operating/0/fund_nm/General+Fund/0/dept_nm/Human+Resources/0/div_nm/Division+placeholder/0/gp_nm/Group+placeholder/0/unit_nm/Tuition+Reimbursement/0/obj_cat/Contractuals/0/obj_cat?vis=barChart',
+    amount: null,
+    explainYourSpending: null,
+    lastYearAmount: 465000,
+    amount2018: 465000,
+    amount2017: 465000,
+    amount2016: 465000,
+    percentChange: 0,
+    deptId: 12,
+    url:
+      'https://data.austintexas.gov/stories/s/Neighborhood-Housing-and-Community-Development/wywe-6mv3',
+  },
+  {
+    name: 'Interfund Transfers',
+    description:
+      'Transfers from general fund departments to other funds like Economic Incentives, Economic Development, Housing Trust Fund and more',
+    learnMore: 'Learn more about Interfund Transfers: ',
+    learnMoreUrl:
+      'http://budget.austintexas.gov/#!/year/2017/operating/0/fund_nm/General+Fund/0/dept_nm/Non-Departmental/0/div_nm/Transfers+and+Other+Requirements/0/gp_nm/Transfers/0/unit_nm/Interfund+Transfers-Other/0/obj_cat/Transfers/0/obj_cat?vis=barChart',
+    amount: null,
+    explainYourSpending: null,
+    lastYearAmount: 7836108,
+    amount2018: 7836108,
+    amount2017: 23744893,
+    amount2016: 23744893,
+    percentChange: 0,
+    deptId: 13,
+  },
+  {
+    name: 'Interdepartmental Charges',
+    description:
+      'Transfers are payments from general fund departments to non-general fund departments for services provided.  Examples include: the 311 call center, IT support, and utility billing support.',
+    learnMore: 'Learn more about Interdepartmental Charges',
+    learnMoreUrl:
+      'http://budget.austintexas.gov/#!/year/2017/operating/0/fund_nm/General+Fund/0/dept_nm/Non-Departmental/0/div_nm/Transfers+and+Other+Requirements/0/gp_nm/Other+Requirements/0/unit_nm/Other+Requirements/0/obj_cat/Contractuals/0/obj_cat?vis=barChart',
+    amount: null,
+    explainYourSpending: null,
+    lastYearAmount: 35227574,
+    amount2018: 35227574,
+    percentChange: 0,
+    deptId: 14,
   },
 ];
 
--- app/src/data/services.js
+++ app/src/data/services.js
@@ -7,41 +7,41 @@ const services = [
   },
   {
     index: 1,
+    title: 'Public Safety',
+    image: 'public_safety.svg',
+    desc:
+      "The community's safety and security is a top priority for City of Austin leaders. Public safety made up nearly 70% of the City's general fund budget, providing for police, fire, EMS and municipal court services",
+    departments: [1, 2, 3, 4],
+    status: 'ready',
+  },
+  {
+    index: 2,
     title: 'Development',
     image: 'development.svg',
     desc:
-      'The City of Savannah provides a mix of services to foster economic vibrancy, ensure the quality of new development and preserve of our natural resources.',
-    departments: [5, 6],
-    status: 'ready',
+      'The City of Austin provides a mix of services to foster economic vibrancy, ensure the quality of new development and preserve of our natural resources.',
+    departments: [5],
     percentChange: null,
     completeSections: 0,
   },
   {
     index: 3,
-    title: 'Infrastructure & Mobility',
-    image: 'infrastructure.svg',
-    desc:
-      'This service group includes the Public Works Department, Chatham Area Transit, and Watershed Protection.',
-    departments: [13, 14],
-  },
-  {
-    index: 4,
-    title: 'Public Safety',
-    image: 'public_safety.svg',
-    desc:
-      "The community's safety and security is a top priority for City of Savannah leaders. Public safety made up nearly 70% of the City's general fund budget, providing for police, fire, EMS and municipal court services",
-    departments: [1, 2, 3, 4],
-  },
-  {
-    index: 5,
     title: 'Community Services',
     image: 'community_services.svg',
     desc:
-      'The City of Savannah is committed to promoting healthy, livable, communities with access to housing and libraries.  In FY 2015-16, Savannah budgeted $19.3 million to the Health and Human Services Department, the Animal Services Department, the Parks and Recreation Department, the Code Department, and the Neighborhood Housing and Community Development Department.',
-    departments: [7, 8, 9, 10, 11],
+      'The city of Austin is committed to promoting healthy, livable, communities with access to housing and libraries.  The budget covers costs for the Health and Human Services Department, the Austin Public Library, the Animal Services Department, the Parks and Recreation Department, the Code Department, Social Service Contracts and the Neighborhood Housing and Community Development Department.',
+    departments: [6, 7, 8, 9, 10, 11],
   },
   {
-    index: 6,
+    index: 4,
+    title: 'Transfers',
+    image: 'infrastructure.svg',
+    desc:
+      'Transfers are payments from general fund departments to non-general fund departments for services provided.  Examples include: the 311 call center, IT support, utility billing support, or professional training.',
+    departments: [12, 13, 14],
+  },
+  {
+    index: 5,
     title: 'Budget Submission',
     image: 'envelope.svg',
     departments: [],
